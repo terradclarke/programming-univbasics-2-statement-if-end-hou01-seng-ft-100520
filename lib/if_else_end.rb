8db22e888a1f839021b5be7d75b97b9b416bcaef
Terranie Sims <terranie67@gmail.com>
2020-09-09 23:24:12 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,12 +1,3 @@
-run_code_inside = true
-puts "Code before if...end"
-if run_code_inside
-  puts "code inside"
-end
-puts "Code after if...end"
-
-
-
 current_time = Time.now
 current_time = current_time.to_i 
 if current_time.even?
